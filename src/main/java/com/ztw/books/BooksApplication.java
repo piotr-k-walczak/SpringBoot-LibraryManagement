adf4accf457d98cab952cb24d0030ae9c5433fad
Piotr Walczak <piotr.k.walczak@outlook.com>
2021-04-14 01:37:27 +0200

    Swagger

--- src/main/java/com/ztw/books/BooksApplication.java
+++ src/main/java/com/ztw/books/BooksApplication.java
@@ -2,8 +2,11 @@ package com.ztw.books;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class BooksApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BooksApplication.class, args);
